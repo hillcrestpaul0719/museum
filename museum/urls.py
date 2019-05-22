a69fa5d6b3af08ecdba3a83d0253ba739e1abe1e
Paul Lee <enigma@enigmatic.network>
2019-05-21 20:30:18 -0400

    minimum viable site

--- museum/urls.py
+++ museum/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('exhibit/<int:exhibit_pk>', views.exhibit, name='exhibit'),
-    path('exhibit/<int:exhibit_pk>/object/<int:object_pk>', views.object, name='object'),
+    path('exhibit/<int:pk>', views.exhibit, name='exhibit'),
+    path('object/<int:pk>', views.object, name='object'),
 ]
--- museum/views.py
+++ museum/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Exhibit, Object
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the museum index.")
+    exhibits = Exhibit.objects.all()
+    return render(request, 'museum/index.html', {'exhibits': exhibits})
+
+def exhibit(request, pk):
+    exhibit = Exhibit.objects.get(pk=pk)
+    start_object = Object.objects.filter(exhibit__pk=exhibit.pk)[0]
+    return render(request, 'museum/exhibit.html', {'exhibit': exhibit, 'start_object': start_object})
+
+def object(request, pk):
+    object = Object.objects.get(pk=pk)
+    prevObj = Object.objects.filter(exhibit=object.exhibit, pk__lt=pk).order_by('-pk')
+    nextObj = Object.objects.filter(exhibit=object.exhibit, pk__gt=pk).order_by('pk')
+    if len(prevObj) == 0:
+        prevObj = None
+    else:
+        prevObj = prevObj[0]
+    if len(nextObj) == 0:
+        nextObj = None
+    else:
+        nextObj = nextObj[0]
+    return render(request, 'museum/object.html', {'object': object, 'prevObj': prevObj, 'nextObj': nextObj})
