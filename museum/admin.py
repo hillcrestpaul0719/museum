fc34ca6f9327c4d8867b04c7ab51d6230aaa3c81
Paul Lee <enigma@enigmatic.network>
2019-06-11 21:12:43 -0400

    add STATIC_ROOT

--- museum/admin.py
+++ museum/admin.py
@@ -4,5 +4,15 @@ from django.contrib import admin
 
 from .models import Exhibit, Object
 
+# def make_published(modeladmin, request, queryset):
+#     queryset.update(status='p')
+# make_published.short_description = "Mark selected stories as published"
+#
+# class ArticleAdmin(admin.ModelAdmin):
+#     list_display = ['title', 'status']
+#     ordering = ['title']
+#     actions = [make_published]
+
+
 admin.site.register(Exhibit)
 admin.site.register(Object)
